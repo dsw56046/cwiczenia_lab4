0c421611993e7163e305cdeb5f870822f6bf5de5
Dominik <dsw56046@student.dsw.edu.pl>
2025-05-28 12:36:57 +0200

    Dodano flagę --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,13 @@
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
 fi
+
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+    count=${2:-100}
+    for i in $(seq 1 "$count"); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Utworzone przez: $0" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+fi
